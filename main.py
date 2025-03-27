bf4edf4907c6158542cd1609d19a0ff26399a1e4
SONIA <soniafirdous1985@gmail.com>
2025-03-27 07:57:23 +0500

    added print function

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
+print("i don't want to push this file on git hub")
 print("aqsa")
 pi=3.14
 r=int(input("enter the radius:"))
-print(f"the area of circle is:{pi*r**2}")
+print(f"the area of circle shapea ball is:{pi*r**2}")
